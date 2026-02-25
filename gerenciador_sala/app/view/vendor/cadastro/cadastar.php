90d2dd07c1f0bbd5face1f47b3ace3800c6f3873
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2026-02-25 09:11:27 -0400

    Add files via upload

---
+++ gerenciador_sala/app/view/vendor/cadastro/cadastar.php
@@ -0,0 +1,48 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="data:," type="image/x-icon" />
+    <link rel="stylesheet" href="app/view/css/global.css" />
+    <link rel="stylesheet" href="app/view/css/component/button.css" />
+    <link rel="stylesheet" href="app/view/vendor/cadastro/css/style.css">
+    <title>login</title>
+    <style>
+        input::-webkit-calendar-picker-indicator {
+            display: none;
+        }
+    </style>
+</head>
+
+<body class="body-center">
+    <div class="center box-border">
+        <h1 class="h1-center">cadastrar</h1>
+        <div class="center">
+            <p style="color: red">
+                <?php if (isset($_SESSION["log_create"]))
+                    echo $_SESSION["log_create"];
+                session_destroy();
+                ?>
+            </p>
+            <form action="/cadastro" method="post" autocomplete="off">
+                <label for="nome">nome</label><br />
+                <input type="text" name="nome" /><br />
+                <label for="email">email</label><br />
+                <input type="email" name="email" /><br />
+                <label for="senha">senha</label><br />
+                <input type="password" name="senha" /><br /><br />
+                <div class="box-center">
+                    <input type="submit" value="enter" class="bt-enter" />
+                    <p></p>
+                <a href="/">login</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
