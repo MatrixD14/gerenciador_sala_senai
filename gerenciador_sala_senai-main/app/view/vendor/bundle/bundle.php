f7d5c250837b8dc95982a08eaef583ff39eb4e9b
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2026-03-05 07:30:08 -0400

    Delete gerenciador_sala_senai-main directory

--- gerenciador_sala_senai-main/app/view/vendor/bundle/bundle.php
+++
@@ -1,4 +0,0 @@
-<script src="/app/view/vendor/layout/js/ajax-router.js"></script>
-<script src="/app/view/vendor/tabelas/js/selectDate.js"></script>
-<script src="/app/view/vendor/layout/js/button.js"></script>
-<script src="/app/view/vendor/tabelas/js/eventClice.js"></script>
--- gerenciador_sala_senai-main/app/view/vendor/tabelas/menuTop/topBar.php
+++
@@ -1,5 +0,0 @@
-<div class="menu-table">
-    <?= $tipoTabela ?>
-    <div class="local-icon-reload">
-        <?= MenuTable::geraMenuTable($tipoTabela, $_SESSION['privilegio']) ?></div>
-</div>
