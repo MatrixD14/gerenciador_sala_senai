90eb9ca29fd47de6b80fb9502f75baa4088f38b3
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2026-03-12 10:42:30 -0400

    Delete gerenciador_sala_senai-main directory

--- gerenciador_sala_senai-main/app/view/vendor/recuperaSenha/verificarToken.php
+++
@@ -1,3 +0,0 @@
-<form action="/checkToken" method="post">
-    <label for="token"></label>
-</form>
--- gerenciador_sala_senai-main/app/view/vendor/tabelas/menuPainel/js/bloqueiaDate.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('focusin', (e) => {
-    if (e.target && e.target.id === 'dia' && e.target.type === 'date') {
-        const hoje = new Date().toISOString().split('T')[0];
-        if (!e.target.min) {
-            e.target.min = hoje;
-        }
-    }
-});
