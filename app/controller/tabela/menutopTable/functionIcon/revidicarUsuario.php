0f1bf46a4c1c030aff91bf95e2df51ee2383a79a
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2026-03-16 10:45:32 -0400

    Delete app directory

--- app/controller/tabela/menutopTable/functionIcon/revidicarUsuario.php
+++
@@ -1,9 +0,0 @@
-<?php
-class revindicar
-{
-    public static function EnviaRevidicacao()
-    {
-        $IdUser = $_POST['id'];
-        $buscaEmail = BuscaInfoUser::buscaIdEmail($IdUser);
-    }
-}
--- app/view/vendor/layout/footer.php
+++
@@ -1,8 +0,0 @@
-    <footer class="footer">
-        <span>© <?= date("Y"); ?> Gerenciador de Sala</span>
-        <span>Versão 1.0</span>
-        <span>Desenvolvido: MatrixD14</span>
-        <span>Desing: Anderson e Claudio</span>
-        <span>Autor: Anderson</span>
-    </footer>
-    <?php require_once __DIR__ . '/../bundle/bundle.php'; ?>
