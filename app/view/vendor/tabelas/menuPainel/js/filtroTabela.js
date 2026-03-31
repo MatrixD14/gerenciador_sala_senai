9789caf735fbdb2e6ac4463ab103e67cfdcd74eb
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2026-03-31 10:33:27 -0400

    Delete app directory

--- app/view/vendor/tabelas/menuPainel/js/filtroTabela.js
+++
@@ -1,8 +0,0 @@
-function filtraTabele(e) {
-  if (e) e.preventDefault();
-  const form = e.currentTarget || e.target;
-  const url = form.getAttribute("action");
-  const formData = new FormData(form);
-  formData.append("is_search_ajax", "true");
-  loadPagePost(url, formData, true);
-}
